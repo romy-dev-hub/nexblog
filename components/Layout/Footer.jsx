8c5744a84214b8b37e3a5525bc6fdb799f4343a8
xiao <roumaissa.hadibi.dev@gmail.com>
2025-08-31 01:48:16 +0100

    i am still living in 2023 don't mind me

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>© 2023 NexBlog. All rights reserved.</p>
+        <p>© 2025 NexBlog. All rights reserved.</p>
       </div>
     </footer>
   );
